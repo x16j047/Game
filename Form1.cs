50d799b439bf2e10ef62387a11ab95400b1d8b49
藤﨑 智希 <x16j047@chiba-fjb.ac.jp>
2017-06-02 11:53:42 +0900

    ogkvo

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,46 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        int speed = 2;
+        int movex, movey;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            movey+=speed;
+            movex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            movey-=speed;
+            movex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            movex -= speed;
+            movey = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            movex += speed;
+            movey = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += movex;
+            pictureBox1.Top += movey;
         }
     }
 }
